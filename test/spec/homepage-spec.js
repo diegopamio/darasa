176ba1c87a856dd3e96ab34021399c9f76f9fd73
Diego Pamio <diego.pamio@intel.com>
2014-05-16 18:11:19 -0300

    Trying back to duplicity.

--- test/spec/homepage-spec.js
+++ test/spec/homepage-spec.js
@@ -12,26 +12,30 @@ i18n.configure({
 });
 
 describe('La home page', function() {
-    browser.get('/');
 
     describe('debería tener un botón para crear un nuevo item que', function(){
         it('exista', function() {
+            browser.get('/');
             expect(element(by.id('shareItem')).isPresent()).to.eventually.be.true
         });
         it('se llame "' + i18n.__('Add New Item') + '"', function(){
+            browser.get('/');
             expect(element(by.id('shareItem')).getText()).to.eventually.equal(i18n.__('Add New Item'))
         });
     })
 
     it('debería tener un título para saber de qué se trata:', function() {
+        browser.get('/');
         expect(element(by.id('titleLabel')).isPresent()).to.eventually.be.true
     });
 
     it('debería tener un "About" para poder saber quién la hizo', function() {
+        browser.get('/');
         expect(element(by.id('aboutButton')).isPresent()).to.eventually.be.true
     });
 
     it('debería tener un botón de "Home" para poder volver al inicio facilmente', function() {
+        browser.get('/');
         expect(element(by.id('aboutButton')).isPresent()).to.eventually.be.true
     });
 
