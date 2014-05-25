5d5d61bc3ea01421631bf277f2e576d818a52a32
Diego Pamio <diego.pamio@intel.com>
2014-05-25 17:21:30 -0300

    Made controllers "minification friendly" in darasa.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,14 @@
+/*global angular:false*/
+
 var app = angular.module("darasa-app", ['ngSailsBind', "xeditable"]);
 
 app.run(function (editableOptions, editableThemes) {
+    'use strict'
     editableThemes.bs3.inputClass = 'input-sm';
     editableThemes.bs3.buttonsClass = 'btn-sm';
     editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
 });
-app.controller("ClassroomListCtrl", function ($scope, $sailsBind) {
+app.controller("ClassroomListCtrl", [ "$scope", "$sailsBind", function ($scope, $sailsBind) {
     'use strict';
 
     $scope.newClassroom = {};
@@ -14,5 +17,5 @@ app.controller("ClassroomListCtrl", function ($scope, $sailsBind) {
         $scope.classrooms.push($scope.newClassroom);
         $scope.newClassroom = {};
     };
-});
+}]);
 
